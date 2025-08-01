0d337167451f9641240b9ffd053a83378d38bda9
kambana4200 <99521696+kambana4200@users.noreply.github.com>
2025-08-01 16:57:33 +0200

    Update dt-property.js
    Add apayload to property

--- src/dt-property/dt-property.js
+++ src/dt-property/dt-property.js
@@ -7,15 +7,25 @@ module.exports = function (RED) {
         this.aId = config.aId;
         this.aType = config.aType;
         var node = this;
+
         this.on('input', function (msg, send, done) {
-            node.value = msg.payload;
+            // Remplacer aContext par la valeur de msg.payload
+            node.aContext = msg.payload;
+
+            // Construction de l'objet avec la valeur mise à jour
             var data = {
-                payload: node,
+                payload: {
+                    accessGroup: node.accessGroup,
+                    aContext: node.aContext,
+                    aId: node.aId,
+                    aType: node.aType,
+                },
             };
+
             send(data);
+            if (done) done(); // appel à done si défini
         });
     }
-    ;
+
     RED.nodes.registerType('dt-property', DTProperty);
 };
-//# sourceMappingURL=dt-property.js.map
